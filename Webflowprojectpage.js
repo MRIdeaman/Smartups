c63476f6dcca1264ecaf8c910984352e08fb7535
MRIdeaman <pieter_piete1@yahoo.com.au>
2022-02-22 14:27:08 +1100

    trying to get creator name to appear on project page

--- Webflowprojectpage.js
+++ Webflowprojectpage.js
@@ -32,6 +32,10 @@ request.onload = function() {
         const h1 = item.getElementsByTagName('H1')[0]
         h1.textContent = data.IdeaName;       
 
+        //test to get creators name on card
+         const h4 = card.getElementsByTagName('H4')[0]
+         h4.textContent = restaurant.YourName;
+
          // For each restaurant, create an paragraph and set the text content to the restaurant's description
                 const p = item.getElementsByTagName('P')[0]
                 p.textContent = `${data.YourIdea.substring(0, 240)}` // Limit to 240 chars
